fc85087899eb3203d9e2cf623b69d6865f029813
Vinay Jain <vinay.jn7@gmail.com>
2015-07-11 23:42:08 +0530

    Merged with remote repo

--- HealthHack/ParseStarterProject/QuestionsListController.m
+++ HealthHack/ParseStarterProject/QuestionsListController.m
@@ -45,6 +45,11 @@
 
 #pragma mark - Table view data source
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+ 
+    return 0;
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return [self.questions count];
 }
