52b8f8b59ed73e4cdeb42e133f049bfbe9d2d51a
Vinay Jain <vinay.jn7@gmail.com>
2015-07-11 17:23:23 +0530

    Added QuestionsListController

---
+++ HealthHack/ParseStarterProject/QuestionsListController.h
@@ -0,0 +1,13 @@
+//
+//  QuestionsListController.h
+//  ParseStarterProject
+//
+//  Created by Vinay Jain on 7/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionsListController : UITableViewController
+
+@end
