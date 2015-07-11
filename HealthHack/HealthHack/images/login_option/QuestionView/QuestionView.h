8999530d6c613680fdbb0135efd6f068f0e045ba
Pravin Tate <Pravin@Pravins-MacBook-Pro.local>
2015-07-11 17:19:44 +0530

    Question view GUI done

---
+++ HealthHack/HealthHack/images/login_option/QuestionView/QuestionView.h
@@ -0,0 +1,13 @@
+//
+//  QuestionView.h
+//  HealthHack
+//
+//  Created by Pravin Tate on 7/11/15.
+//  Copyright © 2015 Vinay Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionView : UIViewController
+
+@end
