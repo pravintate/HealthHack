ab86faedeac37f05ad0c389c22b5d08f7c5e5e29
Vinay Jain <vinay.jn7@gmail.com>
2015-07-11 15:50:13 +0530

    Removed unnecessary code

--- HealthHack/ParseStarterProject/ParseStarterProjectAppDelegate.h
+++ HealthHack/ParseStarterProject/ParseStarterProjectAppDelegate.h
@@ -6,12 +6,6 @@
 
 #import <Foundation/Foundation.h>
 
-@class ParseStarterProjectViewController;
-
 @interface ParseStarterProjectAppDelegate : NSObject <UIApplicationDelegate>
-
 @property (nonatomic, strong) IBOutlet UIWindow *window;
-
-@property (nonatomic, strong) IBOutlet ParseStarterProjectViewController *viewController;
-
 @end
---
+++ HealthHack/ParseStarterProject/ViewController.m
@@ -0,0 +1,19 @@
+//
+//  ViewController.m
+//  ParseStarterProject
+//
+//  Created by Vinay Jain on 7/11/15.
+//
+//
+
+#import "ViewController.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+@end
