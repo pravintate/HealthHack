13ea6a001aa94c5f7352dd91b91c3154080683cb
Vinay Jain <vinay.jn7@gmail.com>
2015-07-11 12:20:15 +0530

    initial Commit

---
+++ HealthHack/HealthHack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthHack
+//
+//  Created by Vinay Jain on 7/11/15.
+//  Copyright © 2015 Vinay Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
